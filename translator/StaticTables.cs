972b30a90b9cd89234a0bbcd8001d82931842103
adyev <adyev.dm@yandex.ru>
2020-02-29 19:59:39 +0700

    Static_table

--- translator/StaticTables.cs
+++ translator/StaticTables.cs
@@ -1,10 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace translator
 {
     class StaticTables
     {
+        List<string> lexems;
+
+        
+        public StaticTables(string file)
+        {
+            lexems = new List<string>();
+            using (StreamReader sr = new StreamReader(file))
+            { 
+                while (!sr.EndOfStream)
+                    this.Add(sr.ReadLine());     
+            }
+            lexems.Sort();
+        }
+        void Add(string element)
+        {
+            if (!lexems.Contains(element))
+            {
+                lexems.Add(element);
+            }
+        }
+        public string GetName(int num)
+
+        {
+            if((num -1 <= lexems.Count)&&(num-1 >= 0))
+                return lexems[num-1];
+            return null;
+        }
+        public int GetNumber(string name)
+        {
+            if (lexems.Contains(name))
+                return lexems.IndexOf(name)+1;
+            
+            return 0;
+        }
     }
 }
